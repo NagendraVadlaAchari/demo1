1fe640772ed7fefa751486776a7272871ab400d8
NagendraVadlaAchari <49055147+NagendraVadlaAchari@users.noreply.github.com>
2019-07-15 18:36:29 +0530

    Update DemoSpringBoot1Application.java

--- src/main/java/com/example/demo/DemoSpringBoot1Application.java
+++ src/main/java/com/example/demo/DemoSpringBoot1Application.java
@@ -7,13 +7,18 @@ import java.io.IOException;
 import javax.security.auth.Subject;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -26,11 +31,14 @@ import com.filenet.api.core.Folder;
 import com.filenet.api.core.ObjectStore;
 import com.filenet.api.util.UserContext;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @RestController
+@EnableSwagger2
 public class DemoSpringBoot1Application {
 
-	
+	private static final Logger logger = LoggerFactory.getLogger(DemoSpringBoot1Application.class);
 	static java.util.Properties props = new java.util.Properties();
 	// Properties props = null;
 	static Connection connection = null;
@@ -63,7 +71,8 @@ public class DemoSpringBoot1Application {
 		return objectstore;
 	}
 
-	@GetMapping("/")
+	@GetMapping("/index")
+//	@RequestMapping(method = RequestMethod.GET, value = "/api/javainuse")
 	public ModelAndView indexPage() {
 		return new ModelAndView("index");
 	}
@@ -76,7 +85,7 @@ public class DemoSpringBoot1Application {
 	 * ModelAndView("welcome", map); }
 	 */
 	
-	@GetMapping("/Name/{name}")
+	@PostMapping("/Name/{name}")
 	public String getName1(@PathVariable String name) {
 		System.out.println("getName api is called {}" + name);
 		
@@ -114,22 +123,25 @@ public class DemoSpringBoot1Application {
 	
 	@GetMapping("/Name/{name}/empId/{empid}/department/{dept}")
 	///empName/{empname1}/empId/{empId1}/department/{department1}
-	public String getName1(@PathVariable String name,@PathVariable String empid, @PathVariable String dept) {
+	public String getName1(@PathVariable String name,@PathVariable String empid, @PathVariable String dept) throws JSONException {
 		System.out.println("getName api is called {____________}" + name);
-
+		logger.info("getName api is called {____________}" + name);
+		JSONArray array=new JSONArray();
+		JSONObject object=new JSONObject();	
 		try {
 
 		//	name = null;
 			DemoSpringBoot1Application connectToFilenet = new DemoSpringBoot1Application();
 			Connection connection2 = connectToFilenet.getConnection();
 			System.out.println(connection2 + "...connection2...");
+			logger.info(connection2 + "...connection2...");
 			Domain domain2 = connectToFilenet.getDomain();
 			System.out.println(domain2.get_Name() + "...domain2...");
+			logger.info(domain2.get_Name() + "...domain2...");
 			ObjectStore objectStore2 = connectToFilenet.getObjectStore();
 			System.out.println(objectStore2 + "...objectStore2...");
+			logger.info(objectStore2 + "...objectStore2...");
 			
-			JSONArray array=new JSONArray();
-			JSONObject object=new JSONObject();	
 
 			 Folder folderInstance = Factory.Folder.createInstance(objectStore2, "AO_Employee");
 				Folder get_RootFolder = objectStore2.get_RootFolder();
@@ -141,9 +153,10 @@ public class DemoSpringBoot1Application {
 			//	folderInstance.getProperties().putValue("AO_EmailID", "chinna_123");
 				folderInstance.save(RefreshMode.REFRESH);
 			System.out.println("Properties r Updated Successfully.,,");
+			logger.info("Properties r Updated Successfully.,,");
 			//name = "Properties r Updated Successfully..!";
 			
-			object.put("requestId","Req0000010");
+			object.put("requestId","REQ0000010");
 			object.put("empName",name);
 			object.put("empId",empid);
 			object.put("caseId",folderInstance.get_Name());
@@ -151,21 +164,35 @@ public class DemoSpringBoot1Application {
 			
 			array.put(object); 
 			System.out.println(array.toString());
+			logger.info("...array.toString()..."+array.toString());
 			return array.toString();
 
 		} catch (Exception e) {
 			// TODO: handle exception
 			System.out.println("error in catch block..!" + e.getMessage());
-			name = e.getMessage();
+			name = "Fail"+e.getMessage();
+			logger.info("catch block error-"+name);
+			
+			
+			object.put("caseStatus","Fail");
+			object.put("errorDetails",name);
+			//object.put("empId",empid);
+		
+			array.put(object); 
+			System.out.println(array.toString());
+			return array.toString();
+			
+		//	return name;
 		}
 
 		// logger.info("message {}", this.message);
-		return name;
+		//return name;
 		// return "<h1>Success</h1>";
 	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringBoot1Application.class, args);
+		logger.info("inside the main method..!");
 	}
 
 }
